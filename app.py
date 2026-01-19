e101b6a9de8eac28493cffff33be20bacba0bcf0
bautistapulgarin <bautistapulgarin@gmail.com>
2026-01-19 11:07:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ import json
 # Cargar secreto
 google_json = st.secrets["GOOGLE_SHEETS_JSON"]
 
-# Reemplazar cualquier posible salto de línea mal interpretado
-google_json = google_json.replace("\\n", "\n")
-
-# Convertir a diccionario
+# Convertir la cadena JSON en diccionario Python
 credentials_dict = json.loads(google_json)
 
 st.write("JSON cargado correctamente")
