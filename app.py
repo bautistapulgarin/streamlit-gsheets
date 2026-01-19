2b9ce0fbfd407c5cab7bd56c0c8048f468ca4e2d
bautistapulgarin <bautistapulgarin@gmail.com>
2026-01-19 11:05:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import json
 
-# Leer el secreto
+# Cargar secreto
 google_json = st.secrets["GOOGLE_SHEETS_JSON"]
+
+# Reemplazar cualquier posible salto de línea mal interpretado
+google_json = google_json.replace("\\n", "\n")
+
+# Convertir a diccionario
 credentials_dict = json.loads(google_json)
 
+st.write("JSON cargado correctamente")
